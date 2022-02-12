4fb9299754aa6869fdb3bc4bb519103502e1ad17
Abhishek <jagtap.abhishek22@gmail.com>
2022-02-12 15:55:40 +0530

    Moving User to JPA

--- BasicProject/src/main/java/com/ap/BasicProject/controller/UserController.java
+++ BasicProject/src/main/java/com/ap/BasicProject/controller/UserController.java
@@ -3,8 +3,10 @@ package com.ap.BasicProject.controller;
 import com.ap.BasicProject.entity.User;
 import com.ap.BasicProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -12,8 +14,26 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable int id){
+        Optional<User> user = userService.getUserById(id);
+        if (user.isPresent())
+            return user.get();
+        throw new RuntimeException("Invalid User");
+    }
+
     @GetMapping("/user")
-    public User getUser(){
-        return userService.getUser();
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @PostMapping("/user")
+    public User createOrUpdate(@RequestBody User user){
+        return userService.createOrUpdateUser(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUser(@PathVariable int id){
+        userService.deleteUser(id);
     }
 }
--- BasicProject/src/main/java/com/ap/BasicProject/service/UserService.java
+++ BasicProject/src/main/java/com/ap/BasicProject/service/UserService.java
@@ -1,14 +1,32 @@
 package com.ap.BasicProject.service;
 
 import com.ap.BasicProject.entity.User;
+import com.ap.BasicProject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
-    public User getUser() {
-        return new User(1, "Abhi", new Date());
+    @Autowired
+    UserRepository userRepository;
+
+    public Optional<User> getUserById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User createOrUpdateUser(User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(int id){
+        userRepository.deleteById(id);
     }
 }
