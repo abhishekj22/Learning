a9c1fc762821bee7a4d70d19f187fcc3295fb3e4
Abhishek <jagtap.abhishek22@gmail.com>
2022-02-12 12:33:33 +0530

    Hello World with a Path Variable

--- BasicProject/src/main/java/com/ap/BasicProject/controller/BasicController.java
+++ BasicProject/src/main/java/com/ap/BasicProject/controller/BasicController.java
@@ -1,6 +1,7 @@
 package com.ap.BasicProject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class BasicController {
     @GetMapping("/hello")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloWorldWithPath(@PathVariable String name){
+        return "Hello " + name;
     }
 }
