1e642fa8509c34d5728f887f326b2115e969393d
Abhishek <jagtap.abhishek22@gmail.com>
2022-02-16 21:40:04 +0530

    Internalization

--- BasicProject/src/main/java/com/ap/BasicProject/controller/BasicController.java
+++ BasicProject/src/main/java/com/ap/BasicProject/controller/BasicController.java
@@ -3,9 +3,14 @@ package com.ap.BasicProject.controller;
 import com.ap.BasicProject.configuration.PropertiesConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class BasicController {
@@ -15,6 +20,9 @@ public class BasicController {
 
     @Autowired
     PropertiesConfiguration propertiesConfiguration;
+
+    @Autowired
+    MessageSource messageSource;
 
     @GetMapping("/hello")
     public String helloWorld(){
@@ -35,5 +43,15 @@ public class BasicController {
     @GetMapping("/getProp")
     public String getPersonDetails(){
         return "Person name is " + propertiesConfiguration.getName() + " and age is " +propertiesConfiguration.getAge();
+    }
+
+    @GetMapping("/hello-i8n")
+    public String helloWorldInternalization(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("welcome.msg", null, "Default msg", locale);
+    }
+
+    @GetMapping("/hello-i8n-2")
+    public String helloWorldInternalization2(){
+        return messageSource.getMessage("welcome.msg", null, "Default msg", LocaleContextHolder.getLocale());
     }
 }
